5d07ea5e8d46acf103ee97a856b9cdc9be2e4b8d
= <matthewdavis111@gmail.com>
2017-08-12 13:12:04 +0100

    create mocks for test

--- VPC-PowerShell/New-MDAwsVirtualNetwork.Tests.ps1
+++ VPC-PowerShell/New-MDAwsVirtualNetwork.Tests.ps1
@@ -2,6 +2,8 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
 Describe "VPC created with PowerShell" {
+  Mock New-EC2Vpc -MockWith { return @{vpcId = 'myvpcId'}}
+  Mock New-EC2Subnet -MockWith { return @{subNetid = 'mySubnetId'}}
   Context "Tag"{
     $tag =  New-MDEC2Tag -key "Name" -value "London"
     It "Tage has correct key" {
